14498409d651135590676bb85f7cadd8e58e1c00
Kristóf Szénási <szenasi.kristof@outlook.com>
2023-01-27 21:55:14 +0100

    Add project files.

---
+++ lotteryGamesInterfaces/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace lotteryGamesInterfaces
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lottery5 = new System.Windows.Forms.Button();
+            this.lottery6 = new System.Windows.Forms.Button();
+            this.lottery7 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lbNumbers = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnResetLottery7 = new System.Windows.Forms.Button();
+            this.btnResetLottery6 = new System.Windows.Forms.Button();
+            this.btnResetLottery5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lottery5
+            // 
+            this.lottery5.AutoEllipsis = true;
+            this.lottery5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(177)))), ((int)(((byte)(210)))), ((int)(((byte)(182)))));
+            this.lottery5.Font = new System.Drawing.Font("Gadugi", 9F, System.Drawing.FontStyle.Bold);
+            this.lottery5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(92)))), ((int)(((byte)(90)))));
+            this.lottery5.Image = ((System.Drawing.Image)(resources.GetObject("lottery5.Image")));
+            this.lottery5.ImageAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.lottery5.Location = new System.Drawing.Point(16, 44);
+            this.lottery5.Margin = new System.Windows.Forms.Padding(0);
+            this.lottery5.Name = "lottery5";
+            this.lottery5.Size = new System.Drawing.Size(120, 75);
+            this.lottery5.TabIndex = 0;
+            this.lottery5.TabStop = false;
+            this.lottery5.Text = "5 Numbers Lottery";
+            this.lottery5.UseVisualStyleBackColor = false;
+            this.lottery5.Click += new System.EventHandler(this.lottery5_Click);
+            // 
+            // lottery6
+            // 
+            this.lottery6.AutoEllipsis = true;
+            this.lottery6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(177)))), ((int)(((byte)(210)))), ((int)(((byte)(182)))));
+            this.lottery6.Font = new System.Drawing.Font("Gadugi", 9F, System.Drawing.FontStyle.Bold);
+            this.lottery6.ForeColor = System.Drawing.Color.BlueViolet;
+            this.lottery6.Image = ((System.Drawing.Image)(resources.GetObject("lottery6.Image")));
+            this.lottery6.ImageAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.lottery6.Location = new System.Drawing.Point(177, 44);
+            this.lottery6.Margin = new System.Windows.Forms.Padding(0);
+            this.lottery6.Name = "lottery6";
+            this.lottery6.Size = new System.Drawing.Size(120, 75);
+            this.lottery6.TabIndex = 1;
+            this.lottery6.TabStop = false;
+            this.lottery6.Text = "6 Numbers Lottery";
+            this.lottery6.UseVisualStyleBackColor = false;
+            this.lottery6.Click += new System.EventHandler(this.lottery6_Click);
+            // 
+            // lottery7
+            // 
+            this.lottery7.AutoEllipsis = true;
+            this.lottery7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(177)))), ((int)(((byte)(210)))), ((int)(((byte)(182)))));
+            this.lottery7.Font = new System.Drawing.Font("Gadugi", 9F, System.Drawing.FontStyle.Bold);
+            this.lottery7.ForeColor = System.Drawing.Color.DarkOrange;
+            this.lottery7.Image = ((System.Drawing.Image)(resources.GetObject("lottery7.Image")));
+            this.lottery7.ImageAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.lottery7.Location = new System.Drawing.Point(338, 44);
+            this.lottery7.Margin = new System.Windows.Forms.Padding(0);
+            this.lottery7.Name = "lottery7";
+            this.lottery7.Size = new System.Drawing.Size(120, 75);
+            this.lottery7.TabIndex = 2;
+            this.lottery7.TabStop = false;
+            this.lottery7.Text = "7 Numbers Lottery";
+            this.lottery7.UseVisualStyleBackColor = false;
+            this.lottery7.Click += new System.EventHandler(this.lottery7_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 10;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Font = new System.Drawing.Font("Gadugi", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(16, 210);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 9;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(300, 150);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // lbNumbers
+            // 
+            this.lbNumbers.AutoSize = true;
+            this.lbNumbers.Font = new System.Drawing.Font("Gadugi", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNumbers.ForeColor = System.Drawing.Color.Snow;
+            this.lbNumbers.Location = new System.Drawing.Point(12, 172);
+            this.lbNumbers.Name = "lbNumbers";
+            this.lbNumbers.Size = new System.Drawing.Size(283, 24);
+            this.lbNumbers.TabIndex = 3;
+            this.lbNumbers.Text = "Select Your Lottery Numbers";
+            this.lbNumbers.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gadugi", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Snow;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(251, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Select Your Lottery Game";
+            // 
+            // btnResetLottery7
+            // 
+            this.btnResetLottery7.AutoEllipsis = true;
+            this.btnResetLottery7.BackColor = System.Drawing.Color.RosyBrown;
+            this.btnResetLottery7.FlatAppearance.BorderSize = 0;
+            this.btnResetLottery7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnResetLottery7.Font = new System.Drawing.Font("Gadugi", 8F, System.Drawing.FontStyle.Bold);
+            this.btnResetLottery7.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnResetLottery7.Location = new System.Drawing.Point(338, 122);
+            this.btnResetLottery7.Margin = new System.Windows.Forms.Padding(0);
+            this.btnResetLottery7.Name = "btnResetLottery7";
+            this.btnResetLottery7.Size = new System.Drawing.Size(120, 24);
+            this.btnResetLottery7.TabIndex = 6;
+            this.btnResetLottery7.Text = "Reset Ticket";
+            this.btnResetLottery7.UseVisualStyleBackColor = false;
+            this.btnResetLottery7.Visible = false;
+            this.btnResetLottery7.Click += new System.EventHandler(this.btnResetLottery7_Click);
+            // 
+            // btnResetLottery6
+            // 
+            this.btnResetLottery6.AutoEllipsis = true;
+            this.btnResetLottery6.BackColor = System.Drawing.Color.RosyBrown;
+            this.btnResetLottery6.FlatAppearance.BorderSize = 0;
+            this.btnResetLottery6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnResetLottery6.Font = new System.Drawing.Font("Gadugi", 8F, System.Drawing.FontStyle.Bold);
+            this.btnResetLottery6.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnResetLottery6.Location = new System.Drawing.Point(177, 122);
+            this.btnResetLottery6.Margin = new System.Windows.Forms.Padding(0);
+            this.btnResetLottery6.Name = "btnResetLottery6";
+            this.btnResetLottery6.Size = new System.Drawing.Size(120, 24);
+            this.btnResetLottery6.TabIndex = 7;
+            this.btnResetLottery6.Text = "Reset Ticket";
+            this.btnResetLottery6.UseVisualStyleBackColor = false;
+            this.btnResetLottery6.Visible = false;
+            this.btnResetLottery6.Click += new System.EventHandler(this.btnResetLottery6_Click);
+            // 
+            // btnResetLottery5
+            // 
+            this.btnResetLottery5.AutoEllipsis = true;
+            this.btnResetLottery5.BackColor = System.Drawing.Color.RosyBrown;
+            this.btnResetLottery5.FlatAppearance.BorderSize = 0;
+            this.btnResetLottery5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnResetLottery5.Font = new System.Drawing.Font("Gadugi", 8F, System.Drawing.FontStyle.Bold);
+            this.btnResetLottery5.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnResetLottery5.Location = new System.Drawing.Point(16, 122);
+            this.btnResetLottery5.Margin = new System.Windows.Forms.Padding(0);
+            this.btnResetLottery5.Name = "btnResetLottery5";
+            this.btnResetLottery5.Size = new System.Drawing.Size(120, 24);
+            this.btnResetLottery5.TabIndex = 8;
+            this.btnResetLottery5.Text = "Reset Ticket";
+            this.btnResetLottery5.UseVisualStyleBackColor = false;
+            this.btnResetLottery5.Visible = false;
+            this.btnResetLottery5.Click += new System.EventHandler(this.btnResetLottery5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(978, 599);
+            this.Controls.Add(this.btnResetLottery5);
+            this.Controls.Add(this.btnResetLottery6);
+            this.Controls.Add(this.btnResetLottery7);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbNumbers);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.lottery7);
+            this.Controls.Add(this.lottery6);
+            this.Controls.Add(this.lottery5);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lottery Ticket Maker v1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button lottery5;
+        private System.Windows.Forms.Button lottery6;
+        private System.Windows.Forms.Button lottery7;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label lbNumbers;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnResetLottery7;
+        private System.Windows.Forms.Button btnResetLottery6;
+        private System.Windows.Forms.Button btnResetLottery5;
+    }
+}
+
---
+++ lotteryGamesInterfaces/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lotteryGamesInterfaces
+{
+    public partial class Form1 : Form
+    {
+        int selectedNumbers = 0;
+        int howManyNumbers = 0;
+        int columns = 0;
+        int rows = 0;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void ticketDesign()
+        {
+            tableLayoutPanel1.Size = new Size(50 * columns + 10 * (columns - 1) , 35 * rows + 10 * (rows - 1));
+            tableLayoutPanel1.ColumnCount = columns;
+            tableLayoutPanel1.RowCount = rows;
+
+            for (int i = 1; i <= tableLayoutPanel1.ColumnCount; i++)
+            {
+                tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle());
+            }
+
+            for (int i = 1; i <= tableLayoutPanel1.RowCount; i++)
+            {
+                tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            }
+
+            for(int i = 1; i <= columns * rows; i++)
+            {
+                CheckBox cb = new CheckBox();
+                cb.TextAlign = ContentAlignment.MiddleCenter;
+                cb.Appearance = Appearance.Button;
+                cb.FlatStyle = FlatStyle.Flat;
+                cb.FlatAppearance.BorderSize = 0;
+                cb.FlatAppearance.CheckedBackColor = Color.Snow;
+                cb.ForeColor = Color.DarkOrange;
+                cb.BackColor = Color.SeaGreen;
+                cb.Size = new Size(50,35);
+                cb.Text = "" + i;
+                cb.Click += makeLotteryTicket;
+                tableLayoutPanel1.Controls.Add(cb); 
+            }
+        }
+
+        void labelVisibility()
+        {
+            lbNumbers.Visible = true;
+        }
+
+        void lottery5Visibility()
+        {
+            btnResetLottery5.Visible= true;
+            btnResetLottery6.Visible = false;
+            btnResetLottery7.Visible= false;
+        }
+
+        void lottery6Visibility()
+        {
+            btnResetLottery6.Visible = true;
+            btnResetLottery5.Visible = false;
+            btnResetLottery7.Visible= false;
+        }
+
+        void lottery7Visibility()
+        {
+            btnResetLottery7.Visible = true;
+            btnResetLottery5.Visible = false;
+            btnResetLottery6.Visible= false;
+        }
+
+        void makeLotteryTicket(object sender, EventArgs e)
+        {
+            CheckBox cb = (CheckBox)sender;
+
+            if (cb.Checked) selectedNumbers++; else selectedNumbers--;
+            if (selectedNumbers == howManyNumbers)
+            {
+                foreach (Control c in tableLayoutPanel1.Controls)
+                {
+                    if (!(c as CheckBox).Checked) c.Enabled = false;
+                }
+            }
+            else
+            {
+                foreach (Control c in tableLayoutPanel1.Controls)
+                {
+                    c.Enabled = true;
+                }
+            }
+        }
+
+        private void lottery5_Click(object sender, EventArgs e)
+        {
+            tableLayoutPanel1.Controls.Clear();
+            selectedNumbers = 0;
+            howManyNumbers = 5;
+            columns = 10;
+            rows = 9;
+            ticketDesign();
+            labelVisibility();
+            lottery5Visibility();
+        }
+
+        private void lottery6_Click(object sender, EventArgs e)
+        {
+            tableLayoutPanel1.Controls.Clear();
+            selectedNumbers = 0;
+            howManyNumbers = 6;
+            columns = 9;
+            rows = 5;
+            ticketDesign();
+            labelVisibility();
+            lottery6Visibility();
+        }
+
+        private void lottery7_Click(object sender, EventArgs e)
+        {
+            tableLayoutPanel1.Controls.Clear();
+            selectedNumbers = 0;
+            howManyNumbers = 7;
+            columns = 7;
+            rows = 5;
+            ticketDesign();
+            labelVisibility();
+            lottery7Visibility();
+        }
+
+        private void btnResetLottery5_Click(object sender, EventArgs e)
+        {
+            foreach (CheckBox cb in tableLayoutPanel1.Controls.OfType<CheckBox>())
+            {
+                if (cb.Enabled)
+                {
+                    if (selectedNumbers >= 0 || selectedNumbers <= 5)
+                    {
+                        if (cb.Checked == true)
+                        { 
+                            tableLayoutPanel1.Controls.Clear();
+                            lottery5_Click(sender, e);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnResetLottery6_Click(object sender, EventArgs e)
+        {
+            foreach (CheckBox cb in tableLayoutPanel1.Controls.OfType<CheckBox>())
+            {
+                if (cb.Enabled)
+                {
+                    if (selectedNumbers >= 0 || selectedNumbers <= 6)
+                    {
+                        if (cb.Checked == true)
+                        {
+                            tableLayoutPanel1.Controls.Clear();
+                            lottery6_Click(sender, e);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnResetLottery7_Click(object sender, EventArgs e)
+        {
+            foreach (CheckBox cb in tableLayoutPanel1.Controls.OfType<CheckBox>())
+            {
+                if (cb.Enabled)
+                {
+                    if (selectedNumbers >= 0 || selectedNumbers <= 7)
+                    {
+                        if (cb.Checked == true)
+                        {
+                            tableLayoutPanel1.Controls.Clear();
+                            lottery7_Click(sender, e);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
